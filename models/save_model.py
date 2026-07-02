233c3f3c3b0cc03a4ecbde66c17cca31965d395c
zeinab <49367777+zeinab-khj@users.noreply.github.com>
2026-07-02 18:21:07 +0330

    Update save_model.py

--- models/save_model.py
+++ models/save_model.py
@@ -1,1 +1,20 @@
+import joblib
+from pathlib import Path
 
+
+def save_model(model, model_name="svm_pipeline.pkl"):
+
+    model_dir = Path("models")
+    model_dir.mkdir(exist_ok=True)
+
+    model_path = model_dir / model_name
+
+    joblib.dump(model, model_path)
+
+    return model_path
+
+def load_model(model_name="svm_pipeline.pkl"):
+
+    model_path = Path("models") / model_name
+
+    return joblib.load(model_path)
